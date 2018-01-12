30372b44768f98de9b25e0ea432e211098254658
DESKTOP-REKI20R\Usource <dmagyary@siteworx.com>
2018-01-12 14:00:40 +0200

    Added tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from tests import config
 
-class BasePage():
+class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
